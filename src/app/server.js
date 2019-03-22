86f78746adefdcaf81047bffc4416f3fac19c4c0
Ratan Kumar <its.ratan25@gmail.com>
2019-03-22 14:42:36 +0530

    update for server.js

--- src/app/server.js
+++ src/app/server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/employee-portal-ui'));
+app.use(express.static('./dist/{{employee-portal-ui}}'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/employee-portal-ui/index.html'));
+res.sendFile(path.join(__dirname,'/dist/{{employee-portal-ui}}/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000, () => { 
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
-});
+app.listen(process.env.PORT || 8080);
